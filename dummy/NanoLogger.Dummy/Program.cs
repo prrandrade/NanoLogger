657a94070ef2b3cd316930c48a4ad2d9c02905ea
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2020-08-17 21:54:30 -0300

    FIrst commit with base code

---
+++ dummy/NanoLogger.Dummy/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NanoLogger.Dummy
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddNanoLogger();
+            var provider = serviceCollection.BuildServiceProvider();
+            
+            var logger = provider.GetService<ILogger<Program>>();
+            logger.LogInformation("test!");
+        }
+    }
+}
---
+++ src/NanoLogger/NanoLoggerExtensions.cs
@@ -0,0 +1,59 @@
+﻿namespace NanoLogger
+{
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using PropertyRetriever;
+    using PropertyRetriever.Interfaces;
+    using Serilog;
+    using Serilog.Exceptions;
+
+    public static class NanoLoggerExtensions
+    {
+        public static IServiceCollection AddNanoLogger(this IServiceCollection @this)
+        {
+            @this.AddPropertyRetriever();
+
+            bool withSeqLog, withConsoleLog, withFileLog;
+            string serviceName, seqAddress, seqApiKey;
+
+            using (var scope = @this.BuildServiceProvider().GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var propertyRetriever = scope.ServiceProvider.GetService<IPropertyRetriever>();
+
+                serviceName = propertyRetriever.RetrieveServiceName();
+
+                seqAddress = propertyRetriever.RetrieveFromEnvironment<string>("seqLogAddress", null);
+                seqApiKey = propertyRetriever.RetrieveFromEnvironment<string>("seqApiKey", null);
+
+                withSeqLog = propertyRetriever.CheckFromCommandLine("withSeqLog");
+                withConsoleLog = propertyRetriever.CheckFromCommandLine("withConsoleLog");
+                withFileLog = propertyRetriever.CheckFromCommandLine("withFileLog");
+            }
+
+            var logger = new LoggerConfiguration()
+                .Enrich.WithExceptionDetails()
+                .Enrich.WithMachineName()
+                .Enrich.WithProcessId()
+                .Enrich.WithThreadId();
+
+            if (withSeqLog && !string.IsNullOrWhiteSpace(seqAddress) && !string.IsNullOrWhiteSpace(seqApiKey))
+                logger = logger.WriteTo.Seq(serverUrl: seqAddress, apiKey: seqApiKey);
+
+            if (withFileLog)
+                logger = logger.WriteTo.File(path: $"log\\log_{serviceName}_.txt", rollingInterval: RollingInterval.Hour, shared: true);
+
+            if (withConsoleLog)
+                logger = logger.WriteTo.Console();
+
+            Log.Logger = logger.CreateLogger();
+
+            @this.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.ClearProviders();
+                loggingBuilder.AddSerilog(dispose: true);
+            });
+            return @this;
+        }
+    }
+}
